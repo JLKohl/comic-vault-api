826affb315570dbffe6ceba3edcb5fc8e286c005
JLKohl <121449936+JLKohl@users.noreply.github.com>
2026-04-07 17:27:52 -0500

    Adding more cors permission to the server file

--- server.js
+++ server.js
@@ -38,7 +38,10 @@ try {
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: true
+}));
 app.use(express.json());
 
 app.use(
